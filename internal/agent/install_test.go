c2e6b89f7fa7efbb98f9514605cb2dde55df990e
Dimitris Karakasilis <dimitris@karakasilis.me>
2023-05-25 16:28:18 +0300

    [WIP] Try to write a test
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- internal/agent/install_test.go
+++ internal/agent/install_test.go
@@ -2,9 +2,14 @@ package agent
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/kairos-io/kairos/v2/pkg/config"
+	v1 "github.com/kairos-io/kairos/v2/pkg/types/v1"
+	v1mock "github.com/kairos-io/kairos/v2/tests/mocks"
+	"github.com/twpayne/go-vfs"
+	"github.com/twpayne/go-vfs/vfst"
 	"gopkg.in/yaml.v3"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -41,9 +46,85 @@ var _ = Describe("prepareConfiguration", func() {
 
 	It("cleans up the configuration file after context is done", func() {
 		source, err := prepareConfiguration(ctx, url)
+		Expect(err).ToNot(HaveOccurred())
 		cancel()
 
 		_, err = os.Stat(source)
 		Expect(os.IsNotExist(err))
+	})
+})
+
+var _ = Describe("RunInstall", func() {
+	var installConfig v1.RunConfig
+	var userCloudConfigFile, resultFile *os.File
+	var options map[string]string
+	var err error
+	var fs vfs.FS
+	var cleanup func()
+	var cloudInit *v1mock.FakeCloudInitRunner
+
+	BeforeEach(func() {
+		userCloudConfigFile, err = os.CreateTemp("", "kairos-config")
+		Expect(err).ToNot(HaveOccurred())
+		err = userCloudConfigFile.Close()
+		Expect(err).ToNot(HaveOccurred())
+
+		resultFile, err = os.CreateTemp("", "kairos-config")
+		Expect(err).ToNot(HaveOccurred())
+		err = resultFile.Close()
+		Expect(err).ToNot(HaveOccurred())
+
+		userConfig := fmt.Sprintf(`
+#cloud-config
+
+install:
+  image: test
+stages:
+  before-install:
+  - name: "Create a file"
+    commands:
+      - |
+        echo "this was run" > %s
+`, resultFile.Name())
+
+		os.WriteFile(userCloudConfigFile.Name(), []byte(userConfig), os.ModePerm)
+
+		fs, cleanup, err = vfst.NewTestFS(map[string]interface{}{})
+		Expect(err).Should(BeNil())
+
+		cloudInit = &v1mock.FakeCloudInitRunner{}
+
+		installConfig = v1.RunConfig{
+			Debug: false,
+			Config: v1.Config{
+				Logger:          v1.NewNullLogger(),
+				Fs:              vfs.NewReadOnlyFS(fs),
+				CloudInitRunner: cloudInit,
+			},
+			CloudInitPaths: []string{userCloudConfigFile.Name()},
+		}
+
+		options = map[string]string{
+			"cc": userConfig,
+		}
+	})
+
+	AfterEach(func() {
+		cleanup()
+		os.RemoveAll(userCloudConfigFile.Name())
+		os.RemoveAll(resultFile.Name())
+	})
+
+	FIt("respects the user defined after-install stages", func() {
+		// TODO:
+		// - Refactor RunInstall to accept the installConfig pat (/etc/elemental)
+		//   as part of `options` (caller still passes the default "/etc/elemental")
+		// - Pass an installConfig here and a cloud-config that defines an after-install stage
+		// - Check that the after-install stage code has run (check the result of that stage?)
+		err = RunInstall(&installConfig, options)
+		Expect(err).ToNot(HaveOccurred())
+		out, err := os.ReadFile(resultFile.Name())
+		Expect(err).ToNot(HaveOccurred())
+		Expect(string(out)).To(Equal("this was run"))
 	})
 })
