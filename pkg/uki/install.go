24c65affd0e704349cfdaa4e85ec8facbd0ec81a
Itxaka <itxaka@kairos.io>
2023-09-19 12:33:53 +0200

    debug
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- pkg/uki/install.go
+++ pkg/uki/install.go
@@ -3,8 +3,9 @@ package uki
 import (
 	hook "github.com/kairos-io/kairos-agent/v2/internal/agent/hooks"
 	"github.com/kairos-io/kairos-agent/v2/pkg/config"
+	"github.com/kairos-io/kairos-agent/v2/pkg/elemental"
 	v1 "github.com/kairos-io/kairos-agent/v2/pkg/types/v1"
-	elementalUtils "github.com/kairos-io/kairos-agent/v2/pkg/utils"
+	"github.com/kairos-io/kairos-agent/v2/pkg/utils"
 	events "github.com/kairos-io/kairos-sdk/bus"
 )
 
@@ -18,15 +19,44 @@ func NewInstallAction(cfg *config.Config, spec *v1.InstallUkiSpec) *InstallActio
 }
 
 func (i *InstallAction) Run() (err error) {
+	e := elemental.NewElemental(i.cfg)
+	cleanup := utils.NewCleanStack()
+	defer func() { err = cleanup.Cleanup(err) }()
 	// Run pre-install stage
-	_ = elementalUtils.RunStage(i.cfg, "kairos-uki-install.pre")
+	_ = utils.RunStage(i.cfg, "kairos-uki-install.pre")
 	_ = events.RunHookScript("/usr/bin/kairos-agent.uki.install.pre.hook")
 
 	// Get source (from spec?)
-	// Create EFI partition (fat32), we already create the efi partition on normal efi install,w e can reuse that?
+	// Create EFI partition (fat32), we already create the efi partition on normal efi install,we can reuse that?
 	// Create COS_OEM/COS_PERSISTANT if set (optional)
 	// I guess we need to set sensible default values here for sizes? oem -> 64Mb as usual but if no persistent then EFI max size?
 	// if persistent then EFI = source size * 2 (or maybe 3 times! so we can upgrade!) and then persistent the rest of the disk?
+
+	// Deactivate any active volume on target
+	err = e.DeactivateDevices()
+	if err != nil {
+		return err
+	}
+	// Partition device
+	err = e.PartitionAndFormatDevice(i.spec)
+	if err != nil {
+		return err
+	}
+
+	err = e.MountPartitions(i.spec.GetPartitions().PartitionsByMountPoint(false))
+	if err != nil {
+		return err
+	}
+	cleanup.Push(func() error {
+		return e.UnmountPartitions(i.spec.GetPartitions().PartitionsByMountPoint(true))
+	})
+
+	// Before install hook happens after partitioning but before the image OS is applied
+	//err = i.installHook(cnst.BeforeInstallHook, false)
+	//if err != nil {
+	//	return err
+	//}
+
 	// Store cloud-config in TPM or copy it to COS_OEM?
 	// Create dir structure
 	//  - /EFI/Kairos/ -> Store our efi images
@@ -38,7 +68,7 @@ func (i *InstallAction) Run() (err error) {
 	// Set default entry to the one we just created
 	// Probably copy efi utils, like the Mokmanager and even the shim or grub efi to help with troubleshooting?
 
-	_ = elementalUtils.RunStage(i.cfg, "kairos-uki-install.after")
+	_ = utils.RunStage(i.cfg, "kairos-uki-install.after")
 	_ = events.RunHookScript("/usr/bin/kairos-agent.uki.install.after.hook") //nolint:errcheck
 
 	return hook.Run(*i.cfg, i.spec, hook.AfterUkiInstall...)
