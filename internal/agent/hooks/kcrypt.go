0ae9c04eb4e1fe373d5f9d71baa94f8f8e4ef39b
Itxaka <itxaka.garcia@spectrocloud.com>
2024-03-07 15:11:51 +0100

    Make sure kcrypt hook umounts persistent (#244)

--- internal/agent/hooks/kcrypt.go
+++ internal/agent/hooks/kcrypt.go
@@ -2,17 +2,27 @@ package hook
 
 import (
 	"github.com/kairos-io/kairos-agent/v2/pkg/config"
+	"github.com/kairos-io/kairos-agent/v2/pkg/constants"
 	v1 "github.com/kairos-io/kairos-agent/v2/pkg/types/v1"
 	"github.com/kairos-io/kairos-sdk/machine"
 	kcrypt "github.com/kairos-io/kcrypt/pkg/lib"
+	"path/filepath"
 )
 
 type Kcrypt struct{}
 
 func (k Kcrypt) Run(c config.Config, _ v1.Spec) error {
-
 	if len(c.Install.Encrypt) == 0 {
 		return nil
+	}
+	c.Logger.Logger.Info().Msg("Running encrypt hook")
+
+	// We need to unmount the persistent partition to encrypt it
+	// we dont know the state here so we better try
+	err := machine.Umount(filepath.Join("/dev/disk/by-label", constants.PersistentLabel)) //nolint:errcheck
+	if err != nil {
+		c.Logger.Errorf("could not unmount persistent partition: %s", err)
+		return err
 	}
 
 	// Config passed during install ends up here, so we need to read it
@@ -30,6 +40,6 @@ func (k Kcrypt) Run(c config.Config, _ v1.Spec) error {
 			}
 		}
 	}
-
+	c.Logger.Logger.Info().Msg("Finished encrypt hook")
 	return nil
 }
