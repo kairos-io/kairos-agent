b176b47f5609b2154a2d70efa7d6b78b9a38d7ae
Itxaka <itxaka.garcia@spectrocloud.com>
2024-06-07 16:45:50 +0200

    Copy any found sysextensions into active+passive efi dir (#372)

--- internal/agent/hooks/hook.go
+++ internal/agent/hooks/hook.go
@@ -33,6 +33,7 @@ var FirstBoot = []Interface{
 
 // AfterUkiInstall sets which Hooks to run after uki runs the install action
 var AfterUkiInstall = []Interface{
+	&SysExtPostInstall{},
 	&Lifecycle{},
 }
 
