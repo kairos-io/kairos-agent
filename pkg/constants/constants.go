a45cee2ebcba95c55e4811465753143fa927baf2
Dimitris Karakasilis <dimitris@karakasilis.me>
2024-09-26 14:59:16 +0300

    Fix order or directories
    so that we allow users to override baked-in livecd configuration with
    config coming from datasource (written in /oem)
    
    Signed-off-by: Dimitris Karakasilis <dimitris@karakasilis.me>

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -178,11 +178,11 @@ func GetYipConfigDirs() []string {
 
 func GetUserConfigDirs() []string {
 	return []string{
-		"/oem",
-		"/usr/local/cloud-config",
 		"/run/initramfs/live",
 		"/etc/kairos",    // Default system configuration file https://github.com/kairos-io/kairos/issues/2221
 		"/etc/elemental", // for backwards compatibility
+		"/usr/local/cloud-config",
+		"/oem",
 	}
 }
 
