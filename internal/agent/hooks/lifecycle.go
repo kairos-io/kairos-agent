faf996453b5721e55b3143a207e365538bef9aa6
Itxaka <itxaka@kairos.io>
2025-10-24 11:01:40 +0200

    feat: Enhance installation completion messages and lifecycle delays
    - Updated installation completion message to clarify reboot/shutdown options based on finish action.
    - Changed sleep duration in lifecycle hooks from 5 milliseconds to 5 seconds for better clarity.
    - Improved user feedback during installation process by adjusting exit messages based on finish action.
    - Block the keys on reboot/shutdown when install finishes so users cant break the installer action by mistake
    
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- internal/agent/hooks/lifecycle.go
+++ internal/agent/hooks/lifecycle.go
@@ -1,10 +1,11 @@
 package hook
 
 import (
+	"time"
+
 	"github.com/kairos-io/kairos-agent/v2/pkg/config"
 	v1 "github.com/kairos-io/kairos-agent/v2/pkg/types/v1"
 	"github.com/kairos-io/kairos-sdk/utils"
-	"time"
 )
 
 type Lifecycle struct{}
@@ -12,12 +13,12 @@ type Lifecycle struct{}
 func (s Lifecycle) Run(c config.Config, spec v1.Spec) error {
 	c.Logger.Logger.Debug().Msg("Running Lifecycle hook")
 	if spec.ShouldReboot() {
-		time.Sleep(5)
+		time.Sleep(5 * time.Second)
 		utils.Reboot()
 	}
 
 	if spec.ShouldShutdown() {
-		time.Sleep(5)
+		time.Sleep(5 * time.Second)
 		utils.PowerOFF()
 	}
 	c.Logger.Logger.Debug().Msg("Finish Lifecycle hook")
